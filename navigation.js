3b6dcdf904e270a5ac37129d962adb931236f130
mjjstockman <mjjstockman@gmail.com>
2023-09-28 08:53:35 +0100

    Add nav

---
+++ navigation.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const toggleButton = document.querySelector('[data-hs-collapse="#navbar-collapse-with-animation"]');
+    const navbarCollapse = document.getElementById("navbar-collapse-with-animation");
+  
+    toggleButton.addEventListener("click", function () {
+      navbarCollapse.classList.toggle("hidden");
+    });
+  });
+  
